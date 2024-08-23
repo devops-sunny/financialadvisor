a37991bf683c8975b28e9de5c2608205b8bc55cf
dhrupadpatel <dhrupad1912@gmail.com>
2024-08-23 09:35:26 +0530

    add

--- controllers/financialAdvisorController.js
+++ controllers/financialAdvisorController.js
@@ -1,4 +1,8 @@
 const FinancialAdvisor = require('../models/financialAdvisorModel');
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 exports.getAllFinancialAdvisors = async (req, res) => {
   try {
@@ -12,7 +16,7 @@ exports.getAllFinancialAdvisors = async (req, res) => {
 exports.getFinancialAdvisorById = async (req, res) => {
   try {
     const advisor = await FinancialAdvisor.findById(req.params.id).populate('userId');
-    if (!advisor) return res.status(404).json({ message: 'Financial Advisor not found' });
+     if (!advisor) return res.status(404).json({ message: 'Financial Advisor not found' });
     res.status(200).json(advisor);
   } catch (err) {
     res.status(400).json({ error: err.message });
@@ -21,6 +25,11 @@ exports.getFinancialAdvisorById = async (req, res) => {
 
 exports.createFinancialAdvisor = async (req, res) => {
   try {
+    const { name, email } = req.body;
+
+    const hashedPassword = await bcrypt.hash(email, 12);
+    const newUser = await User.create({ name, email, password: hashedPassword });
+    
     const newAdvisor = await FinancialAdvisor.create(req.body);
     res.status(201).json(newAdvisor);
   } catch (err) {
