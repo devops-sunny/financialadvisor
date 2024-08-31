cd72b9a6479a840c22bd5c087af8ffef63bbbd06
dhrupadpatel <dhrupad1912@gmail.com>
2024-08-31 14:26:09 +0530

    add the text

--- client/src/sections/auth/Login.js
+++ client/src/sections/auth/Login.js
@@ -1,12 +1,15 @@
 // @mui
 import { Alert, Tooltip, Stack, Typography, Link, Box } from '@mui/material';
 // auth
+import { Link as RouterLink } from 'react-router-dom';
+
 import { useAuthContext } from '../../auth/useAuthContext';
 // layouts
 import LoginLayout from '../../layouts/login';
 //
 import AuthLoginForm from './AuthLoginForm';
 import AuthWithSocial from './AuthWithSocial';
+import { PATH_AUTH } from '../../routes/paths';
 
 // ----------------------------------------------------------------------
 
@@ -21,7 +24,7 @@ export default function Login() {
         <Stack direction="row" spacing={0.5}>
           <Typography variant="body2">New user?</Typography>
 
-          <Link variant="subtitle2">Create an account</Link>
+          <Link variant="subtitle2" component={RouterLink} to={PATH_AUTH.register}>Create an account</Link>
         </Stack>
 
         <Tooltip title={method} placement="left">
