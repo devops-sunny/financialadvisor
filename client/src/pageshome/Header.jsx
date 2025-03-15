c2e74d78cff563c7ca1aa30aa34fcff5054e49b1
dhrupad <drcpythonmfe@gmail.com>
2025-03-15 19:21:24 +0530

    add  change rout path

--- client/src/pageshome/Header.jsx
+++ client/src/pageshome/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
 
             {/* Right Section - Button */}
             <div className="ak-main-header-right">
-              <Link to="/contact" className="common-btn">
+              <Link to="/register" className="common-btn">
                 <span> Get Started</span>
                 <span>
                   <svg
